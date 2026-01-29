fa7142e2070602300fccba0d6f04067ceba2aab9
aquaticasura <nikvid08@gmail.com>
2026-01-29 14:27:24 +0100

    save

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Splines.Interpolators;
 
 public class Cauldron : MonoBehaviour, IPointerClickHandler
 {
+    public string[] ingredients;
+    HotbarController hotbarcontroller;
     public void OnPointerClick(PointerEventData eventData)
     {
+        int i = 0; 
         Debug.Log("CauldronClciked");
+        ingredients[0] = hotbarcontroller.itemhold;
+        i++;
+        
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hotbarcontroller = GetComponent<HotbarController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
     }
+
+
 }
